cdaabcef2f46881fd7ed096a5c158f1e94f631b0
Moreo734 <anthony64gp@gmail.com>
2026-01-22 14:27:28 -0800

    Starting to work on the project

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import random
 import check_input
+from check_input import get_int_range
 
 shellNum = random.randint(1,3)
 wallet = 100
@@ -8,9 +9,10 @@ sentinelCHar = "Y"
 
 
 print("--Shell Game --")
-print("\nFind the ball to double your bet amount!")
+print("Find the ball to double your bet amount!")
 
 ##loop will start around here
-while sentinelCHar == "Y":
-    print("\n\nYou have $" + str(wallet) + ".")
+#while sentinelCHar == "Y":
+print("\nYou have $" + str(wallet) + ".")
+check_input.get_int_range("How much would you like to bet?", 0, int(wallet))
 
