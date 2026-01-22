b0ec5cd6a8a0fd743f95336c22c3d94934409c92
Moreo734 <anthony64gp@gmail.com>
2026-01-22 15:29:14 -0800

    Update main.py
    Added the body of the code that will be updated further until completed

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from check_input import get_int_range
 
 shellNum = random.randint(1,3)
 wallet = 100
-sentinelCHar = "Y"
+isRunning = True
 guessNum = 1
+bet = 0
+
 shell_1 = r"""
    ___     ___     ___
   /   \   /   \   /   \
@@ -25,21 +27,36 @@ shell_3= r"""
  /     \ /     \ /  0  \
  ------- ------- -------
 """
+shells = {
+    1: shell_1,
+    2: shell_2,
+    3: shell_3
+}
 
 print("--Shell Game --")
 print("Find the ball to double your bet amount!")
 
 ##loop will start around here
-#while sentinelCHar == "Y":
-print("\nYou have $" + str(wallet) + ".")
-check_input.get_int_range("How much would you like to bet?", 0, int(wallet))
+while isRunning and wallet > 0:
+    print("\nYou have $" + str(wallet) + ".")
+    bet = check_input.get_int_range("How much would you like to bet?", 0, int(wallet))
 
-print(r"""
+    print(r"""
    ___     ___     ___
   /   \   /   \   /   \
  /  1  \ /  2  \ /  3  \
  ------- ------- -------
 """)
 
-check_input.get_int_range("Make a guess:", 1, 3)
-
+    guessNum = check_input.get_int_range("Make a guess:", 1, 3)
+    print(shells[shellNum])
+    if guessNum == shellNum:
+        print("Congratulations! You won!")
+        wallet += bet*2
+    else:
+        print("Sorry, you lost!")
+        wallet -= bet
+    shellNum = random.randint(1,3)
+if wallet == 0:
+    print("You're out of money! Game Over!")
+isRunning = check_input.get_yes_no("Do you want to play again? (Y/N) : ")
