658119ec90ae870f0177fb4455d9c9bdfa77e461
Moreo734 <anthony64gp@gmail.com>
2026-01-22 14:58:07 -0800

    Update main.py
    Added the basic print function and prepped the ascii art necessary

--- main.py
+++ main.py
@@ -6,7 +6,25 @@ from check_input import get_int_range
 shellNum = random.randint(1,3)
 wallet = 100
 sentinelCHar = "Y"
-
+guessNum = 1
+shell_1 = r"""
+   ___     ___     ___
+  /   \   /   \   /   \
+ /  o  \ /     \ /     \
+ ------- ------- -------
+"""
+shell_2= r"""
+   ___     ___     ___
+  /   \   /   \   /   \
+ /     \ /  o  \ /     \
+ ------- ------- -------
+"""
+shell_3= r"""
+   ___     ___     ___
+  /   \   /   \   /   \
+ /     \ /     \ /  0  \
+ ------- ------- -------
+"""
 
 print("--Shell Game --")
 print("Find the ball to double your bet amount!")
@@ -15,4 +33,13 @@ print("Find the ball to double your bet amount!")
 #while sentinelCHar == "Y":
 print("\nYou have $" + str(wallet) + ".")
 check_input.get_int_range("How much would you like to bet?", 0, int(wallet))
+
+print(r"""
+   ___     ___     ___
+  /   \   /   \   /   \
+ /  1  \ /  2  \ /  3  \
+ ------- ------- -------
+""")
+
+check_input.get_int_range("Make a guess:", 1, 3)
 
