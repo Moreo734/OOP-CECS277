afc88fd3768b3e72fcbb7a0d9bcf843c763c94da
Moreo734 <anthony64gp@gmail.com>
2026-01-26 12:08:45 -0800

    This is the framework of the Rock Paper Scissors Program for Lab 2

---
+++ RoShamBo/RoShamBo.py
@@ -0,0 +1,19 @@
+import check_input
+
+def weapon_menu():
+    #This function will let the user select their weapon
+    print("Blank")
+
+def comp_menu():
+    #This function will decide the weapon that the computer will have
+    print("blank")
+
+def find_winner():
+    #This function will decide whether the computer or the human won
+    print("blank")
+
+def main():
+    #This is the main function. It will call all of the previous functions
+    weapon_menu()
+    print("blank")
+
