fc0fb6f9842442192831132cd96ae927e2851eaf
Moreo734 <anthony64gp@gmail.com>
2026-02-02 12:02:53 -0800

    I'm not sure what I am comitting

---
+++ UselessPage.py
@@ -0,0 +1,20 @@
+import random
+def main():
+    x = 3/2
+    print(x)
+    a = 5
+    b = 5
+
+    if a<=b != a ==b:
+        print("true")
+    else:
+        print("false")
+
+    x = square(5)
+    print(x)
+
+def square(num):
+    num** 2
+
+main()
+
