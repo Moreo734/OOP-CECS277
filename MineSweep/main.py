f1a08c1a80ae5a34125f8bd6b29a4f55f221d6e0
Moreo734 <anthony64gp@gmail.com>
2026-02-02 12:13:23 -0800

    Starting work on the minesweeper proejct

---
+++ MineSweep/main.py
@@ -0,0 +1,15 @@
+import check_input
+
+def create_board(rows, cols):
+    #Something will go in here
+def place_mines(board, mines):
+
+def count_mines(board):
+
+def display_board(board):
+
+def main():
+
+
+
+main()
